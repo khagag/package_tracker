b1783f06b387a5ef82e44fffbb3ac7bde4f673b3
khagag <the_lord29@yahoo.com>
2020-09-15 19:35:51 +0200

    [create] admin table migration

--- database/migrations/2020_09_15_173231_create_admins_table.php
+++ database/migrations/2020_09_15_173231_create_admins_table.php
@@ -15,6 +15,11 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('is_super')->default(false);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
