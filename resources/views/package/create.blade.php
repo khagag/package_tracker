7a51ac9a16636b108f942f3a76e83c6c9b7324c9
khagag <the_lord29@yahoo.com>
2020-09-16 11:20:50 +0200

    [add] links to admin sidebar

--- resources/views/package/create.blade.php
+++ resources/views/package/create.blade.php
@@ -9,7 +9,13 @@
           <div class="card-header">Control
           </div>
           <div class="list-group">
-            <a href="" class="list-group-item list-group-item-action">text</a>
+            <a href="{{route('packages.create')}}" class="list-group-item list-group-item-action">Create Package</a>
+          </div>
+          <div class="list-group">
+            <a href="{{route('states.create')}}" class="list-group-item list-group-item-action">Create State</a>
+          </div>
+          <div class="list-group">
+            <a href="{{route('getPackage')}}" class="list-group-item list-group-item-action">update package state</a>
           </div>
           <div class="card-footer">
           </div>
