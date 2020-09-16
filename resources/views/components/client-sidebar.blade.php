8f33629917f7ae52de5d4a659e002d27606f2f19
khagag <the_lord29@yahoo.com>
2020-09-16 14:50:11 +0200

    [create] client sidebar compnent

---
+++ resources/views/components/client-sidebar.blade.php
@@ -0,0 +1,11 @@
+
+  <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+<div class="card">
+  <div class="card-header">Control
+  </div>
+  <div class="list-group">
+    <a href="{{route('packages.search')}}" class="list-group-item list-group-item-action">Create Package</a>
+  </div>
+  <div class="card-footer">
+  </div>
+</div>
